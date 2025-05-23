f181a48e7eac3c90f4a84a7010fbde81118c47dd
Pawel Migdal <pawelmigdal15@gmail.com>
2025-05-23 11:59:01 -0600

    adding a cache buster file

---
+++ scripts/cache-buster.js
@@ -0,0 +1,44 @@
+/**
+ * Cache-buster for SeedLot Alberta application
+ * This script helps prevent browser caching issues when delivering updates to the client
+ */
+
+// Generate a unique version number based on current timestamp
+var version = new Date().getTime();
+
+// Function to add version parameter to a URL
+function addVersionToURL(url) {
+  return url + (url.indexOf("?") >= 0 ? "&" : "?") + "v=" + version;
+}
+
+// Function to dynamically load a JavaScript file with version parameter
+function loadScript(url, callback) {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = addVersionToURL(url);
+
+  // When script is loaded, call the callback if provided
+  if (callback) {
+    script.onload = callback;
+  }
+
+  document.head.appendChild(script);
+}
+
+// Function to dynamically load a CSS file with version parameter
+function loadCSS(url) {
+  var link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.type = "text/css";
+  link.href = addVersionToURL(url);
+  document.head.appendChild(link);
+}
+
+// Load main application CSS
+loadCSS("css/styles.css");
+
+// Load application scripts in the correct order
+document.addEventListener("DOMContentLoaded", function () {
+  // Load the main scripts
+  loadScript("scripts/requireMain.js");
+});
